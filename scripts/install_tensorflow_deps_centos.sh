9d4d138b619c5d243652dc3e5d5c994e74281ca7
Xiaokui Shu <subbyte@gmail.com>
2019-02-28 13:00:15 -0500

    add libtensorflow setup script for CentOS 7.6

---
+++ scripts/install_tensorflow_deps_centos.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+echo "Installing Haskell TensorFlow Library Dependencies on CentOS 7.6"
+echo "=================================="
+
+# Basic environment
+echo "Checking basic tools..."
+sudo yum install curl wget git -y
+
+# stack
+echo "Checking stack environment..."
+curl -sSL https://get.haskellstack.org/ | sh
+
+# Protocol Buffers
+echo "Preparing library compiling environment..."
+sudo yum install autoconf automake libtool unzip gcc-c++ -y
+TMP_DIR=$(mktemp -d)
+pushd $TMP_DIR
+echo "Downloading protobuf..." 
+git clone https://github.com/google/protobuf.git
+sudo rm -rf /usr/local/src/protobuf
+sudo mv protobuf /usr/local/src/
+pushd /usr/local/src/protobuf
+git submodule update --init --recursive
+./autogen.sh
+./configure --prefix=/usr
+make
+sudo make install
+popd
+popd
+
+# snappy
+echo "Installing snappy..."
+sudo yum install snappy -y
+
+# libtensorflow
+TMP_DIR=$(mktemp -d)
+pushd $TMP_DIR
+echo "Downloading libtensorflow..."
+wget https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.9.0.tar.gz
+echo "Installing libtensorflow..."
+tar zxf libtensorflow-cpu-linux-x86_64-1.9.0.tar.gz
+sudo cp -r lib/* /usr/lib64/
+sudo cp -r include/* /usr/include/
+sudo ldconfig
+popd
+
+echo ""
+echo "Finished"
